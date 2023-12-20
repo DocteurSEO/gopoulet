b238d9856fc7af1d1bc0bb0284da4bc231af6726
ChloeMrk <56391670+ChloeMrk@users.noreply.github.com>
2023-12-20 23:16:39 +0100

    mongodb

--- main.tf
+++ main.tf
@@ -23,9 +23,28 @@ resource "aws_security_group" "jenkins" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     }
-  ingress {
+    ingress {
         from_port   = 22
         to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+ 
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "manngodb" {
+  ingress {
+        from_port   = 27017
+        to_port     = 27017
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
@@ -38,7 +57,6 @@ resource "aws_security_group" "jenkins" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
-
 
 
 
